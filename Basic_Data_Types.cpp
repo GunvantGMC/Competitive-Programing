6150795612f81b71758ad8bdf0b597c2d770cbb1
Gunvant <gunvantchandratre.2001@gmail.com>
2022-07-24 00:58:41 +0530

    first commit

---
+++ Basic_Data_Types.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    long long b;
+    cin >> b;
+
+    string c;
+    cin >> c;
+
+    float d;
+    cin >> d;
+
+    long double e;
+    cin >> e;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+    return 0;
+}
---
+++ Input_and_Output.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+
+    while (cin)
+    {
+        int i = 0;
+        cin >> i;
+        sum += i;
+    }
+    cout << sum;
+
+    return 0;
+}
