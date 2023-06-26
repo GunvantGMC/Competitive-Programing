a14f5fe7ab33584443e0f6d8d7126dbf858a415d
GunvantGMC <gunvantchandratre.2001@gmail.com>
2023-06-26 15:27:24 +0530

    Update

--- temp2.py
+++ temp2.py
@@ -5,4 +5,300 @@ def fu(n):
         return (n+fu(n-1))
 
 
-print(fu(5))
+# print(fu(5))
+
+# a=3
+# b=4
+# c=10
+# c =1+b
+# if((a+b+c)<(c+1)):
+#     if((a+b+c)<(c+a)):
+#         a=(c+7)+a
+#     else:
+#         b=(a+5)+c
+#     a=4+b
+# else:
+#     c=2+b
+# print(a+b+c)
+
+# a=2
+# b=0
+# def funn(a,b):
+#     if((a-b)>(b&a) and (a^2)<(b+a)):
+#         b=1+2+b
+#         a = 2+1+b
+#         b=(a+2)+a
+#         return (funn(b,b)+funn(a+1,b))
+#     else:
+#         return b+1
+    
+# print(funn(a,b))
+
+
+
+# a=3
+# b=4
+# def funn(a,b):
+#     if((b-a)>(a-b) and (4&a)>(a-4)):
+#         a = b+3
+#         b=2+1+a
+#         b=2+a+b
+#         return funn(a,b)+b
+#     else:
+#         return b+a
+    
+# print(funn(a,b))
+
+# a=3
+# b=5
+# c=4
+
+# b=(a+11)+b
+# if((b+5+a) > (8+b)):
+#     b=(a+7)+c
+# else:
+#     if((c+a)<(7-c)):
+#         b=(b+1)+c
+#         c=12+a
+#     else:
+#         a=(9+7)+a
+#     a=(10+11)+a
+# print(a+b+c)
+
+# p=9
+# q=5
+# r=4
+# p=p+q
+# if((p+r+q) < (r+q+p)):
+#     q=r*3
+#     if((r+p+q) <(q+r)):
+
+#         q=r+q+r
+#     else:
+#         r=11+r
+# else:
+#     q=12+p
+#     if((p+5+r) < (8+p)):
+#         p=2+1+p
+#         p=11+p
+
+# print(p+q+r)
+
+
+# a=6
+# b=3
+# c=4
+# def funn(a,b,c):
+#     c=c+c+a
+#     a=12+a
+#     a+=b
+#     b=(a+4)&a
+#     return a+b+c
+
+# print(funn(a,b,c))
+
+
+# p=1
+# q=5
+# r=10
+# r=(r&q)^q
+# q+=11
+# r=5+q
+# print(p+q+r)
+
+
+
+
+# a=9
+# b=0
+# def funn(a,b):
+#     if((3^a) > (a+b) and 2>b):
+#         a=2+b+b
+#         a=a+2
+#         a+=1+a
+#         return funn(a,b+a)-a
+#     else:
+#         return a+b-1
+
+# print(funn(a,b))
+
+
+
+
+# a=1
+# b=8
+# def funn(a,b):
+#     if((a^b) > (3-a) and (a-2) < (b&6)):
+#         b+=4
+#         a=b+1
+#         b=3+a+a
+#         return a-funn(b,b)
+#     else:
+#         b=a+2
+#         return a+b-1
+
+# print(funn(a,b))
+
+
+# a=6
+# b=7
+# def funn(a,b):
+#     if(b>a and a>3):
+#         a+=b+1
+#         b=4+a
+#         return a-funn(b,b)
+#     else:
+        
+#         return b-1
+
+# print(funn(a,b))
+
+
+
+
+
+# a=6
+# b=2
+# c=9
+# c+=a+b
+# for c in range(2+4):
+#     a=(4^10)+b
+
+# print(a+b)
+
+
+
+
+
+# a=6
+# b=7
+# c=6
+# a=14+b
+# if(8>c and (b+c) < (8-b)):
+#     a=13+b
+#     if((b+c+a) < (a+b)):
+#         a=a+c+b
+#     else:
+#         a=15^b
+# else:
+#     b+=c
+#     if((b^a^c) < (c^b)):
+#         c+=8
+
+# print(a+b+c)
+
+
+
+
+
+# p=4
+# q=2
+# r=5
+# p+=q+p
+# if((q+p) < (r-q) and 3>p):
+#     p+=r+q
+#     if((p+r)+(r+q) > (p+r+q)):
+#         r=q+p
+#     else:
+#         r+=p+p
+#     p=(9&6)+q
+# else:
+#     q=r&r
+#     if((q-5) < (5-q)):
+#         r=12&q
+#         q+=q+7+r
+#     p=(1^11)+q
+
+# print(p+q+r)    
+
+
+
+
+
+
+# a=2
+# b=7
+# c=9
+
+# if(9>c or (b&a) < a):
+#     c=b+b+1
+# else:
+#     a+=c
+#     b=5^b
+# print(a+b+c)    
+
+
+
+
+# a=0
+# b=5
+# c=5
+# b=(c+a)&b
+# if(c<b or (b^a)<a):
+#     c+=12+b
+# if((c+a)<(a-c)):
+#     b+=12+b+b
+    
+# print(a+b+c)    
+
+
+
+
+
+# p=2
+# q=6
+# r=7
+# if(1<r and (q+p)>(p-q)):
+#     p=q+q
+# q=p+11+r
+# if((r-p+q) < q+r):
+#     p=r+r+12
+
+# q=p^r
+# print(p+q+r)    
+
+
+
+
+# p=9
+# q=5
+# r=8
+# for r in range(5,6):
+#     if((p+r) < (r-p) or q<p):
+#         q=p+r
+#     if((r&3)<p):
+#         p=p^r
+#         continue
+#     p+=p
+# print(p+q+r)    
+
+
+
+# p=2
+# q=4
+# r=5
+# for r in range(2,4):
+#     if(r<q and ((r+q) < (6-r))):
+#         p=q+7+p
+#     if((p+r+q) > (q-p)):
+#         q=q+8+p
+        
+#     q=q+5+p
+# print(p+q+r)    
+
+
+# p=2
+# q=4
+# r=5
+# for r in range(2,4):
+#     if(r<q and (r+q) < (6-r)):
+#         p=q+7+p
+#     if((p+q+r) > (q-p)):
+#         q=q+8+p
+#     q=q+5+p
+
+# print(p+q+r)
+
+
+
+
